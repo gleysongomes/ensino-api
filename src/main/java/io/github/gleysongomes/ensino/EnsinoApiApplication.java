2bc3ff856d341cd43c30d10de533ce675ba99024
Gleyson Gomes <fgleysondasilva@gmail.com>
2024-06-09 23:37:55 -0300

    criação da api

---
+++ src/main/java/io/github/gleysongomes/ensino/EnsinoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.gleysongomes.ensino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsinoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsinoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gleysongomes/ensino/EnsinoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gleysongomes.ensino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsinoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
